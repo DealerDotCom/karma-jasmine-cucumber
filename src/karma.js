636ad4770c77ffbdf9c0b50e90c79bf80ed502be
Jon Hoguet <jonathan.hoguet@dealer.com>
2014-10-07 09:46:19 -0400

    split most of the implementation out to jasmine-cucumber in preparation for protractor-jasmine-cucumber

--- src/karma.js
+++ src/karma.js
@@ -1,12 +1,27 @@
 /* jshint node:true */
 
+var path = require('path');
+
 function init (files) {
+  var jasmineCucumberPath = path.join(__dirname, '/../node_modules/jasmine-cucumber/');
     var jasmineAdapterFound = false;
     for (var i = 0; i < files.length; i++){
         // need to inject our adapter after the jasmine adapter so that we can wrap it
         if (files[i].pattern.indexOf('/adapter.js') !== -1){
             files.splice(i + 1, 0, {
-                pattern: __dirname + '/jasmine-cucumber.js',
+                pattern: path.join(jasmineCucumberPath, 'src/jasmine-cucumber.js'),
+                included: true,
+                served: true,
+                watched: false
+            });
+            files.splice(i + 1, 0, {
+                pattern: path.join(jasmineCucumberPath, 'src/jasmine-feature-runner.js'),
+                included: true,
+                served: true,
+                watched: false
+            });
+            files.splice(i + 1, 0, {
+                pattern: path.join(jasmineCucumberPath, 'src/karma-jasmine-runner.js'),
                 included: true,
                 served: true,
                 watched: false
